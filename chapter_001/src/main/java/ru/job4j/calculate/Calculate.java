9263bd79d9b6ecc33e1ed758f90efeb31dc91856
AnastasiaSheremet <anastasia_sheremet@mail.ru>
2019-04-22 16:13:10 +0300

    1.3. JavaDoc. Code convention[#135251]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+* This class prints out "Hallo world"
+*@author Anastasia Sheremet
+*@since 2019.04.22
+*@version 1
+*/
 public class Calculate{
+
+	/**
+	* main method
+	*@param args
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+*Package for calculate task.
+*
+*@author Anastasia Sheremet
+*@version 1
+*@since 2019.04.22
+*/
+
+package ru.job4j.calculate;
