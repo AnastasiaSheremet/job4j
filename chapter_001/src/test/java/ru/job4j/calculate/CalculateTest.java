158916d5ef221548d93bb598ce08b596e9bfd82b
AnastasiaSheremet <anastasia_sheremet@mail.ru>
2019-04-22 17:38:44 +0300

    1.4. JUnit[#135252]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Anastasia Sheremet
+ * @version $Id$
+ * @since 2019.04.22
+ */
+public class CalculateTest {
+ /**
+ * Test echo.
+ */
+
+@Test
+ public void whenTakeNameThenThreeEchoPlusName() {
+    String input = "Anastasia Sheremet";
+    String expect = "Echo, echo, echo : Anastasia Sheremet"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+ }
+
+}
