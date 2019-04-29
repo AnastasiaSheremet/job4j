9053218bb18406d85f93b839d89c447c3629e433
AnastasiaSheremet <anastasia_sheremet@mail.ru>
2019-04-29 16:25:21 +0300

    6.4. Слово начинается с ...[#135228]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import java.sql.Array;
+
+/**
+ * Wrap over string.
+ */
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Check that word starts with prefix.
+     * @param prefix prefix.
+     * @return if the word starts with prefix.
+     */
+    public boolean startWith(String prefix) {
+        boolean result = false;
+        char[] value = prefix.toCharArray();
+        for (int i = 0; i < value.length; i++) {
+            if (data[i] == value[i]) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
