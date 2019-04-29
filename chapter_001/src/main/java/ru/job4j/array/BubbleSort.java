a6ee52f30c9574aa4440d17ae0005bfbf43d6ce4
AnastasiaSheremet <anastasia_sheremet@mail.ru>
2019-04-29 22:10:10 +0300

    6.5. Создать программу для сортировки массива методом перестановки.[#135232]

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * Bubble sort.
+ */
+public class BubbleSort {
+    /**
+     * Check that word starts with prefix.
+     * @param array that should be sorted.
+     * @return sorted array.
+     */
+    public int[] sort(int[] array) {
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BubbleSortTest {
+    @Test
+    public void whenSortArrayWithTenElementsThenSortedArray() {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] input = new int[] {4, 7, 1, 5, 9, 8, 0, 2, 6, 3};
+        int[] result = bubbleSort.sort(input);
+        int[] expect = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertThat(result, Matchers.is(expect));
+    }
+}
