9b99c5d670a0e20d1ae193327a04ced22e4e631c
AnastasiaSheremet <anastasia_sheremet@mail.ru>
2019-06-03 22:55:14 +0300

    4. Используя шаблон проектирования - стратегию[#135294]

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
@@ -0,0 +1,10 @@
+package ru.job4j.pseudo;
+
+public class Triangle implements Shape{
+    @Override
+    public String draw() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("*").append("**").append("***").append("****");
+        return sb.toString();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/pseudo/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *Package for pattern Strategy example testing.
+ *
+ *@author Anastasia Sheremet
+ *@version $Id$
+ *@since 2019.06.03
+ */
+
+package ru.job4j.pseudo;
