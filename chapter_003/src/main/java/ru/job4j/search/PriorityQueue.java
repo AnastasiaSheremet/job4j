e2aba1ae063914aeca245d1773798efe33b2977c
AnastasiaSheremet <anastasia_sheremet@mail.ru>
2019-11-25 22:24:13 +0300

    2. Очередь с приоритетом на LinkedList[#135278]

---
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,24 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        if (tasks.size() == 0) {
+            tasks.add(0, task);
+        } else {
+          for (int i = 0; i <= tasks.size(); i++) {
+              if (task.getPriority() <= tasks.get(i).getPriority()) {
+                  tasks.add(i, task);
+                  break;
+              }
+          }
+        }
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/search/Task.java
@@ -0,0 +1,27 @@
+package ru.job4j.search;
+
+public class Task {
+    private String desc;
+    private int priority;
+
+    public Task(String desc, int priority) {
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
