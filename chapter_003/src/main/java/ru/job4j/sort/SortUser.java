02cde3478bb27d4d2aeb3e8f392926cc370c7555
AnastasiaSheremet <anastasia_sheremet@mail.ru>
2019-12-05 22:27:33 +0300

    2. Сортировка User с использованием Comparator[#135270]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,12 +1,36 @@
 package ru.job4j.sort;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SortUser {
 
     public Set<User> sort(List<User> list) {
         return new TreeSet<>(list);
+    }
+
+    public List<User> sortNameLength(List<User> list) {
+        List<User> sorted = new ArrayList<>(list);
+        sorted.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return o1.getName().length() - o2.getName().length();
+                    }
+                }
+        );
+        return sorted;
+    }
+
+    public List<User> sortByAllFields(List<User> list) {
+        List<User> sorted = new ArrayList<>(list);
+        sorted.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return (o1.getName().compareTo(o2.getName()) != 0) ? o1.getName().compareTo(o2.getName()) : Integer.compare(o1.getAge(), o2.getAge());
+                    }
+                }
+        );
+        return sorted;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -2,6 +2,7 @@ package ru.job4j.sort;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 import static org.hamcrest.core.Is.is;
@@ -19,7 +20,7 @@ public class SortUserTest {
         list.add(yangest);
         list.add(oldest);
         Set<User> result = sUser.sort(list);
-        Set<User> expect = new TreeSet<>();
+        Set<User> expect = new HashSet<>();
         expect.add(yangest);
         expect.add(middle);
         expect.add(oldest);
@@ -37,10 +38,44 @@ public class SortUserTest {
         list.add(yangest);
         list.add(oldest);
         Set<User> result = sUser.sort(list);
-        Set<User> expect = new TreeSet<>();
+        Set<User> expect = new HashSet<>();
         expect.add(yangest);
         expect.add(middle);
         expect.add(oldest);
+        assertThat(result, is(expect));
+    }
+
+    public void whenNameLengsSort() {
+        SortUser sUser = new SortUser();
+        User shortest = new User("Ivan", 17);
+        User longest = new User("Sergey", 17);
+        User middle = new User("Pavel", 48);
+        List<User> list = new ArrayList<>();
+        list.add(middle);
+        list.add(longest);
+        list.add(shortest);
+        List<User> result = sUser.sortNameLength(list);
+        List<User> expect = new ArrayList<>();
+        expect.add(shortest);
+        expect.add(middle);
+        expect.add(longest);
+        assertThat(result, is(expect));
+    }
+
+    public void whenAllFieldsSort() {
+        SortUser sUser = new SortUser();
+        User first = new User("Pavel", 17);
+        User second = new User("Ivan", 27);
+        User third = new User("Pavel", 48);
+        List<User> list = new ArrayList<>();
+        list.add(first);
+        list.add(second);
+        list.add(third);
+        List<User> result = sUser.sortNameLength(list);
+        List<User> expect = new ArrayList<>();
+        expect.add(second);
+        expect.add(first);
+        expect.add(third);
         assertThat(result, is(expect));
     }
 }
